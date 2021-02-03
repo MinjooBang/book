e5eb857118514d08065d0fe321ceb0e38e667805
minjoo <qkdalswn121@gmail.com>
2021-02-03 21:31:59 +0900

    5장 네이버 api oauth test 수정

---
+++ src/main/java/com/mj/book/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.mj.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // Jpa Auditing 활성홭ㄴ
+public class JpaConfig {
+}
--- src/test/java/com/mj/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/mj/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.mj.book.springboot.web;
 
+import com.mj.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
@@ -12,12 +16,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 //Spring boot 버전에 따라 어노테이션이 변동이 있음 특히 2.4 대...
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
+    @WithMockUser(roles = "USER")
     @Test
     public void hello_return() throws Exception{
         String hello = "Hello";
@@ -27,7 +36,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
 
     }
-
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto_return() throws Exception {
         String name = "hello";
