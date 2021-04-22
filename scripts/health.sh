e4019637900392851e53dedad2014cf2d17bfa41
minjoo <qkdalswn121@gmail.com>
2021-04-22 19:45:10 +0900

    무중단 health 배포 스크립트 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo ">Health check !!!"
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
+ABSDIR=$(dirname ${ABSPATH})
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
